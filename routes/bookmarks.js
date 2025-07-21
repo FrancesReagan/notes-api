4b1ba34e2320c258e55919ff7864f3ca8f947897
Frances Reagan James <adtgwen@gmail.com>
2025-07-21 16:13:00 -0400

    added PUT method update a book mark

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -29,3 +29,23 @@ router.post("/", async (req, res) => {
     res.status(400).json(err);
   }
 }); 
+
+// PUT  /api/bookmarks/:id --update a bookmark//
+router.put("/:id", async (req, res) => {
+  try {
+    const bookmarkToUpdate = await Bookmark.findById(req.params.id);
+
+    if(!bookmarkToUpdate) {
+      return res.status(404).json({ message: "No bookmark found with this id"});
+    }
+    // authorization check//
+    if (bookmarkToUpdate.user.toString()!==req.user._id.toString()) {
+      return res.status(403).json({ message: "User is not authorized to update this bookmark."});
+    }
+    const bookmark = await Bookmark.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(bookmark);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
